b496230f8ffa2fd418b542fc51507d07af1d3cb1
Yousif Hazim <105172397+YousifHazim@users.noreply.github.com>
2024-05-04 17:48:08 +0300

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+
+const express = require('express')
+const cors = require('cors')
+const{ db } = require('./db/db')
+const{readdirSync} = require('fs')
+const { route } = require('./routes/transactions')
+const app = express()
+
+require('dotenv').config()
+
+const PORT = process.env.PORT
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+// routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)));
+
+const server = () => {
+    db()
+    app.listen(PORT , () => {
+        console.log('are listening port:' , PORT);
+    })
+}
+
+server()
+
