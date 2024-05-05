03845863ff62baef11d25f0380153b5a689b4c4f
Mazen Saeed <mazensaaed228@gmail.com>
2024-05-05 16:55:14 +0300

    Added user,menu items and sign out

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import styled from "styled-components";
 import bg from './img/bg.png';
-
+import {MainLayout} from './styles/Layouts'
+import Orb from './Components/Orb/Orb'
+import Navigation from "./Components/Navigation/Navigation";
 function App() {
-
-  
   return (
     <AppStyled bg={bg} className="App">
-      
+      <Orb />
+      <MainLayout>
+        <Navigation />
+      </MainLayout>
     </AppStyled>
   );
 }
 
 const AppStyled = styled.div`
   height: 100vh;
-  background-image: url(${props => props.bg})
+  background-image: url(${props => props.bg});
+  positition: relative;
 `;
-
 
 export default App;
