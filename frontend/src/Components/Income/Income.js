9e0611253b9a00428d1c45158877dd473b1e4699
Anas Hamed Ahmed Hamed <111208532+Anas2108@users.noreply.github.com>
2024-05-10 01:39:38 +0300

    Edit Income Page
    Add Button Component
    Add IncomeItem Component to display the items in the page

--- frontend/src/Components/Income/Income.js
+++ frontend/src/Components/Income/Income.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import { InnerLayout } from '../../styles/Layouts';
 import { useGlobalContext } from '../../context/globalContext';
 import Form from '../Form/Form';
+import IncomeItem from '../IncomeItem/IncomeItem';
+
 
 function Income(){
 
-    const{addIncome} = useGlobalContext()
+    const{addIncome,incomes,getIncomes,deleteIncome} = useGlobalContext()
+    useEffect(() =>{
+        getIncomes()
+    }, [])
+
     return(
         <IncomeStyled>
             <InnerLayout>
@@ -16,7 +22,22 @@ function Income(){
                         <Form />
                     </div>
                     <div className="incomes">
-
+                    {incomes.map((income) => {
+                            const {_id, title, amount, date, category, description, type} = income;
+                            return <IncomeItem
+                            key={_id}
+                            id={_id} 
+                            title={title} 
+                            description={description} 
+                            amount={amount} 
+                            date={date} 
+                            type={type}
+                            category={category} 
+                            indicatorColor="var(--color-green)"  
+                            deleteItem={deleteIncome}
+                                
+                            />
+                        })}
                     </div>
                 </div>
 
@@ -27,7 +48,33 @@ function Income(){
 }
 
 const IncomeStyled = styled.div`
-
+display: flex;
+overflow: auto;
+.total-income{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #FCF6F9;
+    border: 2px solid #FFFFFF;
+    box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+    border-radius: 20px;
+    padding: 1rem;
+    margin: 1rem 0;
+    font-size: 2rem;
+    gap: .5rem;
+    span{
+        font-size: 2.5rem;
+        font-weight: 800;
+        color: var(--color-green);
+    }
+}
+.income-content{
+    display: flex;
+    gap: 2rem;
+    .incomes{
+        flex: 1;
+    }
+}
 `;
 
 
